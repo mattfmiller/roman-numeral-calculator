a3e45e6041a16ea5881dcb4d4c32e3867651d28e
Bobby Martin and Matt Miller <matt@>
2018-03-29 11:05:11 -0700

    Finalize business logic.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,28 @@
+//Business Logic
 function romanNumeralConverter(number) {
+  var numArray = number.toString().split('').reverse();
   var numerals = ["I", "V", "X", "L", "C", "D", "M"];
   var romanNumeral = "";
-  var numArray = number.toString().split('').reverse();
   console.log(numArray);
   for (var i = 0; i < numArray.length; i++) {
     if (parseInt(numArray[i]) < 4) {
       for (var j = 0; j < parseInt(numArray[i]); j++) {
-        romanNumeral += numerals[0];
+        romanNumeral += numerals[0 + (2*i)];
       }
     } else if (parseInt(numArray[i]) < 9 && parseInt(numArray[i]) >= 5) {
-      romanNumeral += numerals[1];
       for (var j = 0; j < parseInt(numArray[i])-5; j++) {
-        romanNumeral += numerals[0];
+        romanNumeral += numerals[0 + (2*i)];
       }
+      romanNumeral += numerals[1 + (2*i)];
     } else if (parseInt(numArray[i]) === 4) {
-      romanNumeral += numerals[0] + numerals[1];
+      romanNumeral += numerals[1 + (2*i)] + numerals[0 + (2*i)];
     } else if(parseInt(numArray[i]) === 9) {
-      romanNumeral = numerals[0] + numerals[2];
-    } else if (number[i] === 10) {
-      return "X";
-    } else if (number === 50) {
-      return "L";
-    } else if (number === 100) {
-      return "C";
-    } else if (number === 500) {
-      return "D";
-    } else if (number === 1000) {
-      return "M";
+      romanNumeral += numerals[2 + (2*i)] + numerals[0 + (2*i)] ;
     }
   }
-  console.log(romanNumeral);
+  var romanNumeralArray = romanNumeral.split("").reverse();
+  var outputRomanNumeral = romanNumeralArray.join("");
+  console.log(outputRomanNumeral);
 }
+
+//
