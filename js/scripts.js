20c5866e8fb69c0b8f63992b9a4048650839d367
Bobby Martin and Matt Miller <matt@>
2018-03-29 08:58:01 -0700

    Add romanNumerlaConverter function to convert 1,5,10,50,100,500,1000.

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function romanNumeralConverter(number) {
+  // var numeralOne = I;
+  if (number === 1) {
+    return "I";
+    console.log("I");
+  } else if (number === 5) {
+    return "V";
+    console.log("V");
+  } else if (number === 10) {
+    return "X";
+    console.log("X");
+  } else if (number === 50) {
+    return "L";
+    console.log("L");
+  } else if (number === 100) {
+    return "C";
+    console.log("C");
+  } else if (number === 500) {
+    return "D";
+    console.log("D");
+  } else if (number === 1000) {
+    return "M";
+    console.log("M");
+  }
+}
